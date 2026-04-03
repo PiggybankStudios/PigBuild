8b17a6fa97a630bd64250517feeb0edee23194f7
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-04-02 17:53:25 -0700

    Tiny bit more cleanup to build.bat

--- shell/build.bat
+++ shell/build.bat
@@ -20,19 +20,14 @@ if not exist builder.exe (
 )
 
 popd
-EXIT /B 0
 
+EXIT /B 0
 :BuildAndRun
 
 CALL ..\pig_build\shell\init_msvc.bat msvc_environment.txt
-
-del builder.* > NUL 2> NUL
-del builder_hash.txt > NUL 2> NUL
-del build_script.obj > NUL 2> NUL
-
 echo Compiling build_script.c...
+del builder.* builder_hash.txt build_script.obj > NUL 2> NUL
 cl ..\build_script.c /Fe"builder.exe" /Fd"builder.pdb" /std:clatest /INCREMENTAL:NO /Od /FC /nologo /Zi /I".." /I"..\pig_build" /link Shlwapi.lib"
-
 if "%ERRORLEVEL%"=="0" builder.exe %*
 
 EXIT /B
