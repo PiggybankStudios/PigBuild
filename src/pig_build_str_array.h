0e5b17450a523351579e075a76af3ace73f10598
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2026-04-23 23:07:14 -0700

    Moved AddTag to pig_build_tags.h from pig_build_str_array.h

--- src/pig_build_str_array.h
+++ src/pig_build_str_array.h
@@ -41,14 +41,6 @@ Str* AddStr(StrArray* array, Str newString)
 #define AddStrLit(arrayPntr, strLit)     AddStr((arrayPntr), StrLit(strLit))
 #define AddStrNt(arrayPntr, nullTermStr) AddStr((arrayPntr), MakeStrNt(nullTermStr))
 
-//Since tag macros all contain leading '|' character, we need to slice it off when adding it to a StrArray
-Str* AddTag(StrArray* array, const char* newStringWithLeadingSepChar)
-{
-	Str tagStr = MakeStrNt(newStringWithLeadingSepChar);
-	if (tagStr.length >= 1 && tagStr.chars[0] == '|') { tagStr = StrSliceFrom(tagStr, 1); }
-	return AddStr(array, tagStr);
-}
-
 Str* InsertStr(StrArray* array, Str newString, u64 insertIndex)
 {
 	Str* result = InsertItemArray_Str(array, insertIndex);
--- src/pig_build_tags.h
+++ src/pig_build_tags.h
@@ -139,4 +139,12 @@ bool DoTagsMatch(const StrArray* tagsListPntr, const StrArray* includeTags, cons
 	return true;
 }
 
+//Since tag macros all contain leading '|' character, we need to slice it off when adding it to a StrArray
+Str* AddTag(StrArray* array, const char* newStringWithLeadingSepChar)
+{
+	Str tagStr = MakeStrNt(newStringWithLeadingSepChar);
+	if (tagStr.length >= 1 && tagStr.chars[0] == '|') { tagStr = StrSliceFrom(tagStr, 1); }
+	return AddStr(array, tagStr);
+}
+
 #endif //  _PIG_BUILD_TAGS_H
